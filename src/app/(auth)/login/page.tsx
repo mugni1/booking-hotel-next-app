9ab66dce3e4157d19e9cc36c8ff5057737b19739
mugni1 <asepabdulmugni22@gmail.com>
2025-05-28 15:56:26 +0700

    modified

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,6 @@
+import Login from "@/components/layouts/Login";
+import React from "react";
+
+export default function Page() {
+  return <Login />;
+}
---
+++ src/components/layouts/Login.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import React from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa6";
+
+export default function Login() {
+  return (
+    <section className="min-h-screen container mx-auto flex justify-center items-center px-5">
+      <div className="card w-full md:w-4/12 border border-base-300 bg-base-100 card-xs shadow-md p-3">
+        <div className="card-body">
+          <div className="flex items-center justify-between mb-3">
+            <Image
+              src="/images/logo.png"
+              alt="logo"
+              height={100}
+              width={100}
+              className="w-4/12"
+            />
+            <h2 className="card-title flex justify-center font-bold text-2xl">
+              SignIn
+            </h2>
+          </div>
+          <div className="justify-end card-actions">
+            <button className="btn btn-warning w-full flex items-center">
+              <FaGoogle size={24} /> SignIn with Google
+            </button>
+            <button className="btn btn-warning w-full flex items-center">
+              <FaGithub size={24} /> SignIn with Github
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}

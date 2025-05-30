5c9a5d0ea96bec0c368ac04dd6c5f4bd057368ae
mugni1 <asepabdulmugni22@gmail.com>
2025-05-30 21:25:54 +0700

    modified navbar

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -13,8 +13,9 @@ export default function Navbar() {
   const router = useRouter();
   return (
     <section className="w-full shadow-sm min-h-15 fixed top-0 z-50 bg-black/20 backdrop-blur-lg">
+      {/* navbar start  */}
       <nav className="container mx-auto px-5 h-full py-4 justify-between items-center flex flex-wrap">
-        <div className=" text-warning">
+        <div className=" text-warning navbar-start w-auto md:w-2/12">
           <Link href={"/"}>
             <Image
               src="/images/logo.png"
@@ -25,6 +26,8 @@ export default function Navbar() {
             />
           </Link>
         </div>
+        {/* end navbar start  */}
+        {/* burger menu  */}
         <div className="navbar-end md:hidden">
           {isOpen ? (
             <button
@@ -42,9 +45,12 @@ export default function Navbar() {
             </button>
           )}
         </div>
+        {/* end burger menu  */}
+
+        {/* navbar center  */}
         <div
           className={clsx(
-            "w-full mt-4 md:w-auto md:mt-0  md:block",
+            "w-full mt-4 md:w-fit md:mt-0  md:block navbar-center",
             isOpen ? "block" : "hidden"
           )}
         >
@@ -87,28 +93,48 @@ export default function Navbar() {
                 )}
               </>
             )}
-
-            {data ? (
-              <li>
-                <button
-                  className="btn btn-warning btn-sm"
-                  onClick={() => signOut({ redirectTo: "/login" })}
-                >
-                  Logout
-                </button>
-              </li>
-            ) : (
-              <li>
-                <button
-                  className="btn btn-warning btn-sm"
-                  onClick={() => router.push("/login")}
-                >
-                  Login
-                </button>
-              </li>
-            )}
           </ul>
         </div>
+        {/* end navbar center  */}
+
+        {/* navbar end  */}
+        <div
+          className={clsx(
+            "navbar-end md:block md:w-2/12 w-full pt-2 md:border-none border-t border-black/40",
+            isOpen ? "block" : "hidden"
+          )}
+        >
+          {data ? (
+            <div className="flex items-center justify-between md:justify-end md:gap-2">
+              <div className="flex items-center gap-2">
+                <Image
+                  height={50}
+                  width={50}
+                  src={data?.user?.image || "/images/avatar.svg"}
+                  alt="image.png"
+                  className="h-10 w-auto aspect-square rounded-full border object-cover object-center bg-white text-black"
+                />
+                <b className="text-xs line-clamp-2 text-white">
+                  {data.user.name || "anonnymouse"}
+                </b>
+              </div>
+              <button
+                className="btn btn-sm btn-warning"
+                onClick={() => signOut({ redirectTo: "/login" })}
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <button
+              className="btn btn-warning btn-sm"
+              onClick={() => router.push("/login")}
+            >
+              Login
+            </button>
+          )}
+        </div>
+        {/* end navbar end  */}
       </nav>
     </section>
   );

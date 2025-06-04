7d135f928084f861ec8bba5bcb5bf1e6233e57e8
mugni1 <asepabdulmugni22@gmail.com>
2025-06-04 17:28:05 +0700

    modified

--- src/components/shared/RoomList/index.tsx
+++ src/components/shared/RoomList/index.tsx
@@ -4,7 +4,7 @@ import { getRooms } from "@/services/getRooms";
 
 export default async function RoomList() {
   const { data: rooms, success, message } = await getRooms();
-  console.log(success);
+
   return (
     <section className="w-full h-full container mx-auto px-5 grid grid-cols-2 md:grid-cols-4 gap-5 py-8">
       {success ? (
